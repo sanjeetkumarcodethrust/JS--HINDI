f37f689b8676b171da1ae8d4bd2d65a97ef89900
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-02-06 12:38:42 +0530

    it is javascript

---
+++ variable.js
@@ -0,0 +1,35 @@
+const accountId = 12345
+let accountName = "Sanjeet kumar"
+var accountPassword = "2121"
+accountCity ="Bihar"
+// let accountState;
+// accountId=2
+accountName="bittu"
+accountPassword="676"
+accountCity="patana"
+// console.log(accountId);
+console.table([accountId,accountName,accountPassword,accountCity]);
+// alert("hello worled");
+let score=true
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+let isloggedIn="sanjeet"
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+let someNumber="1234"
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+let value=33
+let negvalue=-value
+console.log(negvalue);
+console.log("2"===2);
+const score1=100
+
+
+
+
+
+
