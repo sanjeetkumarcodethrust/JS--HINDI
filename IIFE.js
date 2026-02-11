70245f4b44795ea7d820a016bd8a1725be0121c3
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-02-11 17:04:59 +0530

    iife in javascript

---
+++ IIFE.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expressions (IIFE) 
+(function sanjeet(){
+    console.log(`Hello World`);
+})();
+
+( (name) =>{
+    console.log(`Hello World 2 ${name}`);
+})("sanjeet")
