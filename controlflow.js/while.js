d10af396a5c585f2215c613a1ab6d05bf95a1b1c
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-02-12 23:17:07 +0530

    while do loop in javascript

---
+++ controlflow.js/while.js
@@ -0,0 +1,17 @@
+let index=0
+while(index<=10){
+   console.log(`value of index is ${index}`);
+   index=index+2;
+}
+let myArray=["thor", "ironman", "hulk", "captain america", "black widow"]
+let i=0
+while(i<myArray.length){
+    const element=myArray[i];
+    console.log(element);
+    i++;
+}
+let count=11
+do{
+    console.log(`value of index is ${count}`);
+    count++
+}while(count<=10)
